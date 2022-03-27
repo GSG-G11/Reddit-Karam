49a017d7c8b72730259aab1b0d00f6ad33d3c2ee
karam-zomlut <zomlutk@gmail.com>
2022-03-27 22:40:36 +0300

    Edit BLOB Data Type

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    image TEXT,
+    image BYTEA,
     password VARCHAR(255) NOT NULL
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT,
-    image TEXT,
+    image BYTEA,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
