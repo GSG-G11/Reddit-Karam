ca2977d94f38a0b431af3c56ab73fe97009655b9
karam-zomlut <zomlutk@gmail.com>
2022-03-27 22:45:58 +0300

    Handle Pages in View Router

--- server/router/view.js
+++ server/router/view.js
@@ -1,7 +1,20 @@
+const { join } = require('path');
+
 const view = require('express').Router();
 
+// Handle Home Page
 view.get('/', (req, res) => {
-    res.send('Home Page');
+    res.sendFile(join(__dirname, '..', '..', 'private', 'index.html'));
+});
+
+// Handle Profile Page
+view.get('/view/:id/profile', (req, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'private', 'profile.html'));
+});
+
+// Handle Signin && Signup Page
+view.get('/sign', (req, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'private', 'sign.html'));
 });
 
 module.exports = view;
